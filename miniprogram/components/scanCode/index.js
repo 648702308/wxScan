c2b66b214bf914e301d757459769b5cf9f866f0b
648702308 <648702308@qq.com>
2021-01-30 16:33:44 +0800

    修改

--- miniprogram/components/scanCode/index.js
+++ miniprogram/components/scanCode/index.js
@@ -11,7 +11,7 @@ Component({
    * 组件的初始数据
    */
   data: {
-
+    scanFunctionIsUseable: true
   },
 
   /**
@@ -19,10 +19,11 @@ Component({
    */
   methods: {
     initdone(e) {
-      console.log(e)
-      setTimeout(()=>{
+      console.log('camera 初始化完成')
+      this.clearAnimation('#animationLine')
+      setTimeout(() => {
         this.start()
-      },300)
+      }, 300)
     },
     // 开始动画
     start() {
@@ -32,17 +33,42 @@ Component({
         { translateY: 240 },
         { translateY: 0 },
       ], 6000, function () {
-        that.clearAnimation('#scanCamera', { translateY: true }, function () {
+        that.clearAnimation('#animationLine', { translateY: true }, function () {
           that.start()
         })
       }.bind(this))
     },
+    // 在扫码识别成功时触发
     scancode(e) {
-
-      console.log(e)
+      const that = this
+      if (this.data.scanFunctionIsUseable) {
+        this.setData({ scanFunctionIsUseable: false })
+        wx.showLoading({
+          mask: true,
+          title: '查询中…'
+        })
+        wx.vibrateLong()
+        this.triggerEvent('scancode', {
+          data: e,
+          fun: function () {
+            wx.hideLoading()
+            that.setData({ scanFunctionIsUseable: true })
+          }
+        })
+      }
     },
+    // 用户不允许使用摄像头时触发
     cameraError() {
 
     }
+  },
+  // 以下是旧式的定义方式，可以保持对 <2.2.3 版本基础库的兼容
+  attached: function () {
+    // 在组件实例进入页面节点树时执行
+    this.clearAnimation('#animationLine')
+  },
+  detached: function () {
+    // 在组件实例被从页面节点树移除时执行
+    this.clearAnimation('#animationLine')
   }
 })
--- miniprogram/pages/saoma/index.js
+++ miniprogram/pages/saoma/index.js
@@ -5,26 +5,24 @@ Page({
 
   },
   onLoad: function () {
-    
+
   },
   onShow() {
-    // wx.nextTick(()=>{
-    //   setTimeout(()=>{
-    //     this.selectComponent('#scanCode').start()
-    //   },500)
-    // })
   },
   donghua() {
-    
+
   },
   scancode(e) {
     // 校验扫描结果，并处理
-    let res = e.detail.result
+    console.log(e.detail.data)
+    setTimeout(() => {
+      e.detail.fun()
+    }, 2000)
   },
   onHide() {
-    
+
   },
-  onUnload(){
-    
+  onUnload() {
+
   }
 })
